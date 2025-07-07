4993ec50617fb443458aee72442289713a83ae6d
Deborah Abang <abangdebbie12@gmail.com>
2025-07-07 17:07:58 +0100

    changed the date settings

--- FRONTEND/src/app.js
+++ FRONTEND/src/app.js
@@ -12,3 +12,16 @@ themeToggler.addEventListener('click', () => {
         icon.classList.add('fa-moon');
     }
 });
+
+//timer function
+const timeDisplay = document.getElementById('time-display');
+
+const timeout = setInterval(() => {
+   const now = new Date();
+   const time = now.toTimeString().split(' ')[0];
+   timeDisplay.textContent = time;
+   const day = now.getDay();
+   const month = now.getMonth();
+   const year = now.getFullYear();
+   console.log(`${day} ${month} ${year}`);
+}, 100);
