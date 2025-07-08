863389e69997a840db0c386f1006cec7e8ef01d0
Deborah Abang <abangdebbie12@gmail.com>
2025-07-08 12:09:47 +0100

    finished the quotes section

--- app.js
+++ app.js
@@ -37,3 +37,51 @@ const getOrdinal = n =>
 const formatted = `${capitalize(days[today.getDay()].toLowerCase())} ${getOrdinal(today.getDate())} ${capitalize(months[today.getMonth()].toLowerCase())} ${today.getFullYear()}`;
 
 dateDisplay.textContent = formatted;
+
+//fetching quotes from external API
+let dataArray = [];  // Initialize an empty array to hold quotes
+const getQuotes = async () => {
+    // Fetch quotes from external API
+    const response = await fetch('https://dummyjson.com/quotes');
+    if (!response.ok) throw new Error(`API error: ${response.status}`);  // Throw error if fetch fails
+    const data = await response.json();  // Parse response JSON
+    dataArray = data.quotes; 
+    
+}
+
+getQuotes().then(() => {
+    let index = 0;
+    
+    
+        // If QuotesArray is empty, log error and return early
+        if (dataArray.length === 0){
+            console.error('Error: dataArray is empty');
+            return;
+        }
+
+        // Get references to quote display elements
+        const quoteDisplay = document.getElementById('quote-display');
+        const quoteAuthor = document.getElementById('quote-author');
+
+        // Function to update quote and author
+        const updateQuote = () => {
+            if (index >= dataArray.length) {
+                index = 0;
+            }
+            const quotes = dataArray[index].quote;
+            const author = dataArray[index].author;
+            if (quoteDisplay && quoteAuthor) {
+                quoteDisplay.textContent = `"${quotes}"`;
+                quoteAuthor.textContent = `${author}`;
+            }
+        };
+
+        // Initial display
+        updateQuote();
+
+        // Increment index and update quote every 5 seconds
+        const interval = setInterval(() => {
+            index++;
+            updateQuote();
+        }, 5000);
+})
